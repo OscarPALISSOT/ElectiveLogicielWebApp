56668fc4f8a728350d829ad87ddcc6311ece479c
Oscar PALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-06-09 14:12:28 +0200

    PL-16 add jwt token module Ms-authentification

---
+++ Ms-authentification/modules/jwt.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const createJWT = (user) => {
+    const token = jwt.sign({
+        user: user
+    }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return token;
+}
+
+const checkJWT = (token) => {
+    let check = false;
+    try {
+        check = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        check = false;
+        console.log(err);
+    }
+    return check;
+}
+
+module.exports = { createJWT: createJWT, checkJWT: checkJWT };
