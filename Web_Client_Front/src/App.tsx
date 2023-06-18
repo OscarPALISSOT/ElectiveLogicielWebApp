39dd86b3f9b793825178a11345e3e283158afd6f
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-06-18 18:38:17 +0200

    PL-32 page connexion

--- Web_Client_Front/src/App.tsx
+++ Web_Client_Front/src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route }  from "react-router-dom";
 import Home from "./Pages/Home/Home.tsx";
 import About from "./Pages/About/About.tsx";
 import Authentication from "./Pages/Authentication/Authentication.tsx";
+import CreateAccount from "./Pages/CreateAccount/CreateAccount.tsx";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About />} />
                 <Route path="/authentication" element={<Authentication />} />
+                <Route path="/createAccount" element={<CreateAccount />} />
             </Routes>
         </Router>
     )
--- Web_Client_Front/src/Pages/CreateAccount/CreateAccount.tsx
+++ Web_Client_Front/src/Pages/CreateAccount/CreateAccount.tsx
@@ -3,25 +3,24 @@ import Btn from "../../Components/Btn/Btn.tsx";
 import React from "react";
 
 
-function Authentication() {
+function CreateAccount() {
 
     const formHandler = (event: React.FormEvent) => {
         event.preventDefault()
-        alert('submit')
+        alert('inscription')
     }
 
     return (
         <>
-            <h1>Authentication</h1>
+            <h1>Inscription</h1>
             <form onSubmit={formHandler}>
                 <InputField name={'username'} label={'Your Username'} type={'text'} placeholder={'username@example.com'}/>
                 <InputField name={'pwd'} label={'Your password'} type={'password'}/>
-                <Btn label={'Se connecter'}/>
+                <InputField name={'pwd'} label={'Confirmez le mdp'} type={'password'}/>
+                <Btn label={'Créer le compte'}/>
             </form>
-
-            <Btn label={'Créer un compte'} link={'/createAccount'}/>
         </>
     )
 }
 
-export default Authentication
+export default CreateAccount
