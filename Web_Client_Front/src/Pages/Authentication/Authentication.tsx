772dffff6c183ce8c1076f734b654d3bdcb5a878
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-06-21 00:12:54 +0200

    PL-32 connexion front

--- Web_Client_Front/src/Pages/Authentication/Authentication.tsx
+++ Web_Client_Front/src/Pages/Authentication/Authentication.tsx
@@ -4,7 +4,8 @@ import React, {useEffect} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router";
 import styles from './Authentication.module.css';
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLock, faRightToBracket, faUser} from "@fortawesome/free-solid-svg-icons";
 
 
 function Authentication() {
@@ -39,17 +40,22 @@ function Authentication() {
         setDisabled(true)
         try {
             const user = (await axios.get(import.meta.env.VITE_URL_MS_USER + '/getUser', {params: {email: inputs.email}})).data.response
-            if (!user){
+            if (!user) {
                 setIncorrectEmail(true)
             } else {
-                await axios.post(import.meta.env.VITE_URL_MS_USER + '/checkUserPassword', null, {params: {email: inputs.email, password: inputs.pwd}})
+                await axios.post(import.meta.env.VITE_URL_MS_USER + '/checkUserPassword', null, {
+                    params: {
+                        email: inputs.email,
+                        password: inputs.pwd
+                    }
+                })
                 const token = (await axios.post(import.meta.env.VITE_URL_MS_AUTH + '/create', null, {
                     params: {
                         userId: user.UserId,
                         email: user.Email,
                         firstName: user.FirstName,
                         lastName: user.LastName,
-                        roles: user.Role.map((role: {'Role': string} ) => role.Role).join(', ')
+                        roles: user.Role.map((role: { 'Role': string }) => role.Role).join(', ')
                     }
                 })).data.token
                 await axios.post(import.meta.env.VITE_URL_MS_USER + '/addTokenToUser', null, {
@@ -64,7 +70,7 @@ function Authentication() {
         } catch (error) {
             setDisabled(false)
             console.log(error);
-            if ((error as any).response.status === 401){
+            if ((error as any).response.status === 401) {
                 setIncorrectPwd(true)
             }
 
@@ -74,24 +80,48 @@ function Authentication() {
 
     return (
         <>
-            <div className={styles.divlogo}>
-                <img className={styles.logo} src="./src/Assets/img/logo.svg" alt=""/>
+            <div className={styles.logoContainer}>
+                <img src="./src/Assets/img/logo.svg" alt=""/>
             </div>
 
-            <form className={styles.connexionForm} onSubmit={handleSubmit}>
-                <InputField name={'email'} label={'Votre identifiant'} type={'text'} placeholder={'username@example.com'} onChange={handleChange} value={inputs.email} style={'primary'}/>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <InputField
+                    name={'email'}
+                    type={'text'}
+                    placeholder={'Email'}
+                    onChange={handleChange}
+                    value={inputs.email}
+                    style={'secondary'}
+                    required={true}
+                    radius={'smooth'}
+                    icon={<FontAwesomeIcon icon={faUser}/>}
+                />
                 {incorrectEmail && <p>Indentifiant inconnu</p>}
-                <InputField name={'pwd'} label={'Your password'} type={'password'} onChange={handleChange} value={inputs.pwd} style={'primary'}/>
+                <InputField
+                    name={'pwd'}
+                    type={'password'}
+                    onChange={handleChange}
+                    value={inputs.pwd}
+                    style={'secondary'}
+                    required={true}
+                    radius={'smooth'}
+                    placeholder={'Mot de passe'}
+                    icon={<FontAwesomeIcon icon={faLock} />}
+                />
                 {incorrectPwd && <p>Mot de passe incorrect</p>}
-                <Btn label={'Se connecter'} disabled={disabled} style={'primary'}/>
+                <Btn label={'Se connecter'} disabled={disabled} style={'dark'} icon={<FontAwesomeIcon icon={faRightToBracket} />}/>
             </form>
 
-            <div className={styles.lineContainer}>
-                <div className={styles.line}></div>
-                <div className={styles.text}>ou</div>
+            <div className={styles.separator}>
+                <div className={styles.lineSeparator}></div>
+                <p className={styles.textSeparator}>Ou</p>
+                <div className={styles.lineSeparator}></div>
             </div>
 
-            <Btn label={'Créer un compte'} style={'secondary'} onClick={() => navigate("/register")}/>
+            <div className={styles.registerBtnContainer}>
+                <Btn label={'Créer un compte'} style={'secondary'} onClick={() => navigate("/register")}/>
+            </div>
+
         </>
     )
 }
