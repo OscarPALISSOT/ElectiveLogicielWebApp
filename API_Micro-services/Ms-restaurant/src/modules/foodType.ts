ecf5814c338dc58ace9c061fcf7fbe5a0d84846d
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-06-24 18:40:03 +0200

    foodtype module ms resto

---
+++ API_Micro-services/Ms-restaurant/src/modules/foodType.ts
@@ -0,0 +1,63 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+/**
+ * Create a food type
+ * @param {string} foodType the food type label to create
+ */
+async function CreateFoodType(foodType: string) {
+  return prisma.foodType.create({
+    data: {
+      foodTypeLabel: foodType,
+    },
+  });
+}
+
+/**
+ * Get a food type
+ * @param {string} foodType the food type label to get
+ */
+async function GetFoodType(foodType: string) {
+    return prisma.foodType.findUnique({
+        where: {
+        foodTypeLabel: foodType,
+        },
+    });
+}
+
+/**
+ * Get all food types
+ */
+async function GetFoodTypes() {
+    return prisma.foodType.findMany();
+}
+
+/**
+ * Delete a food type
+ * @param {string} foodType the food type label to delete
+ */
+async function DeleteFoodType(foodType: string) {
+    return prisma.foodType.delete({
+        where: {
+        foodTypeLabel: foodType,
+        },
+    });
+}
+
+/**
+ * Update a food type
+ * @param {string} foodType the food type label to update
+ * @param {string} newFoodType the new food type label
+ */
+async function UpdateFoodType(foodType: string, newFoodType: string) {
+    return prisma.foodType.update({
+        where: {
+        foodTypeLabel: foodType,
+        },
+        data: {
+        foodTypeLabel: newFoodType,
+        },
+    });
+}
