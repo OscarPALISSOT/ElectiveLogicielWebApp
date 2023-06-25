9dcedb93b344de9bf86164f813274c475ef5cff3
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-06-26 01:53:26 +0200

    create ms evaluation

---
+++ API_Micro-services/Ms-evaluation/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+import * as middlewares from './middlewares';
+import evaluation from './api/Evaluation';
+import MessageResponse from './interfaces/MessageResponse';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, MessageResponse>('/', (req, res) => {
+    res.status(200).json({message: 'Hello world'});
+});
+
+app.use('/api/v1/evaluations', evaluation);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+export default app;
