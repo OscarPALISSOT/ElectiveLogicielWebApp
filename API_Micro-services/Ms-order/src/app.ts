483e43e92143a5226f1b6a45acd8f8ccfd93aded
Paul BUHON <paul.buhon@viacesi.fr>
2023-06-27 14:11:49 +0200

    ms_order final

--- API_Micro-services/Ms-order/src/app.ts
+++ API_Micro-services/Ms-order/src/app.ts
@@ -4,8 +4,9 @@ import helmet from 'helmet';
 import cors from 'cors';
 
 import * as middlewares from './middlewares';
-import api from './api';
+import order from './api/Order';
 import MessageResponse from './interfaces/MessageResponse';
+import * as middlewaresUser from './middlewares/tokenMiddleware';
 
 require('dotenv').config();
 
@@ -17,12 +18,13 @@ app.use(cors());
 app.use(express.json());
 
 app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
+  res.status(200).json({ message: 'Hello world' });
 });
 
-app.use('/api/v1', api);
+//app.use(middlewaresUser.tokenMiddleware);
+//app.use(middlewaresUser.isAdmin);
+
+app.use('/api/v1/order', order);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
