2fe5d813b9ee5fe35811b771cad682920fe99772
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-06-30 00:42:56 +0200

    admin resto

---
+++ Web_Client_Front/src/Pages/RestaurantAdmin/Restaurants/AdminRestaurants.tsx
@@ -0,0 +1,51 @@
+import {useState, useEffect} from 'react';
+import axios from "axios";
+import style from "./AdminRestaurants.module.css";
+import {useNavigate} from "react-router";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import {Restaurant} from "../../../Interfaces/Restaurant.ts";
+import Btn from "../../../Components/Btn/Btn.tsx";
+import AdminItem from "../../../Components/Admin/AdminItem/AdminItem.tsx";
+
+function FoodTypeAdmin() {
+
+    const navigate = useNavigate();
+
+    const [foodTypes, setFoodTypes] = useState<Restaurant[]>([])
+
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_BACK_HOST + import.meta.env.VITE_URL_MS_RESTAURANT + '/getAllRestaurants')
+            .then((response) => {
+                setFoodTypes(response.data.response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [])
+
+    return (
+        <>
+            <div className={style.container}>
+                <h1>Gestion des restaurants</h1>
+                <Btn
+                    label={'Ajouter un restaurant'}
+                    style={'primary'}
+                    rounded={true}
+                    icon={<FontAwesomeIcon icon={faPlus}/> }
+                    onClick={() => navigate('/admin/restaurants/add')}
+                />
+                {foodTypes.map((item) => (
+                    <AdminItem
+                        key={item.restaurantId}
+                        title={item.name}
+                        image={import.meta.env.VITE_BACK_HOST + import.meta.env.VITE_URL_MS_RESTAURANT_FOODTYPE + '/getFoodTypeThumbnail?foodTypeIcons=' + item.thumbnail}
+                    />
+                ))}
+            </div>
+
+        </>
+    )
+}
+
+export default FoodTypeAdmin
