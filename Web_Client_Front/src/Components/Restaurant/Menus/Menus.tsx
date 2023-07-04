245eea43d00cbafe1c91e7fb421c40265b8f0cea
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-07-05 00:36:13 +0200

    list menu

--- Web_Client_Front/src/Components/Restaurant/Menus/Menus.tsx
+++ Web_Client_Front/src/Components/Restaurant/Menus/Menus.tsx
@@ -1,6 +1,9 @@
 import style from "./Menus.module.css";
 import React from "react";
 import {Menu} from "../../../Interfaces/Menu.ts";
+import Btn from "../../Btn/Btn.tsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCartShopping} from "@fortawesome/free-solid-svg-icons";
 
 
 interface MenusProps {
@@ -14,9 +17,14 @@ const Menus: React.FC<MenusProps> = ({Menus}: MenusProps) => {
             {Menus.map((menu) => {
                 return (
                     <div key={menu.menuId} className={style.menuItem}>
-                        <p>{menu.name}</p>
-                        <p>{menu.description}</p>
-                        <p>{menu.price}€</p>
+                        <div className={style.content}>
+                            <div className={style.info}>
+                                <p className={style.title}>{menu.name}</p>
+                                <p>{menu.description}</p>
+                            </div>
+                            <p className={style.price}>{menu.price}€</p>
+                        </div>
+                        <Btn label={'Commander'} style={'yellow'} icon={<FontAwesomeIcon icon={faCartShopping}/> }/>
                     </div>
                 )
             })}
