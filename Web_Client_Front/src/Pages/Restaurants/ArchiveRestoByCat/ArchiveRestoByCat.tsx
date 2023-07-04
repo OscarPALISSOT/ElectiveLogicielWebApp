85cb5d69604a9d2900767b4b1c6ffbd7fcea3cec
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-07-04 23:28:34 +0200

    archive resto by cat

--- Web_Client_Front/src/Pages/Restaurants/ArchiveRestoByCat/ArchiveRestoByCat.tsx
+++ Web_Client_Front/src/Pages/Restaurants/ArchiveRestoByCat/ArchiveRestoByCat.tsx
@@ -3,22 +3,22 @@ import {useEffect, useState} from "react";
 import {Restaurant} from "../../../Interfaces/Restaurant.ts";
 import axios from "axios";
 import setAuthTokenHeader from "../../../Modules/SetToken.ts";
-import style from "./SingleSearch.module.css";
+import style from "./ArchiveRestoByCat.module.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faStar, faXmark} from "@fortawesome/free-solid-svg-icons";
 import {useNavigate} from "react-router";
-//import FeaturedSearchs from "../../../Components/Homepage/FeaturedSearchs/FeaturedSearchs.tsx";
-import FeaturedRestaurants from "../../../Components/Homepage/FeaturedRestaurants/FeaturedRestaurants.tsx";
+import {faCircleChevronLeft} from "@fortawesome/free-solid-svg-icons";
+import NavBar from "../../../Components/NavBar/NavBar.tsx";
+import ListRestaurants from "../../../Components/Restaurant/ListRestaurants/ListRestaurants.tsx";
 
 
-function SingleSearch() {
+function ArchiveRestoByCat() {
 
     const navigate = useNavigate();
 
 
     const {label} = useParams();
 
-    const [featuredRestaurants, setFeaturedRestaurants] = useState<Restaurant[]>([]);
+    const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
 
     useEffect(() => {
         setAuthTokenHeader(localStorage.getItem('JWT_auth_Cesivroo'));
@@ -27,8 +27,7 @@ function SingleSearch() {
                 foodTypeLabel: label
             }
         }).then((response) => {
-            console.log(response.data.response);
-            setFeaturedRestaurants(response.data.response);
+            setRestaurants(response.data.response);
         }).catch((error) => {
             console.log(error);
         })
@@ -38,15 +37,18 @@ function SingleSearch() {
     return (
         <>
             <div className={style.header}>
-                <div className={style.close} onClick={() => navigate('/search')}>
-                    <FontAwesomeIcon icon={faXmark} size={'2xl'}/>
-                </div>
+                <FontAwesomeIcon icon={faCircleChevronLeft} size={'2x'} onClick={() => navigate(-1)}/>
+                <h3 className={style.headerTitle}>{label}</h3>
+            </div>
+            <div className={style.container}>
+                <h3>{restaurants.length > 1 ? restaurants.length + ' résultats' : restaurants.length + ' résultat'}</h3>
+                <ListRestaurants restaurants={restaurants}/>
             </div>
 
-            <FeaturedRestaurants FeaturedRestaurants={featuredRestaurants}/>
+            <NavBar/>
 
         </>
     )
 }
 
-export default SingleSearch
+export default ArchiveRestoByCat
